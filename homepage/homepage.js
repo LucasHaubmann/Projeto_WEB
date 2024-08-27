c40775b846681ca26d7bddebf30c98f7d32eb2b4
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2024-08-27 16:19:05 -0300

    Initial commit

---
+++ homepage/homepage.js
@@ -0,0 +1,18 @@
+alert("ainda ta feio ainda, paciencia")
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+    if(cont > 3){
+        cont = 1
+    }
+    document.getElementById('radio'+cont).checked = true
+}
